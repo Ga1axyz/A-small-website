046a090bbc2e2ac51f718133455adb11a70a62c7
刘志远 <80236363+Ga1axyz@users.noreply.github.com>
2021-05-05 11:16:57 -0500

    Add files via upload

--- galaxy.js
+++ galaxy.js
@@ -1,27 +1,12 @@
+var heihei = 0;
+
 window.addEventListener('keydown',caidan);
-
 function caidan (e){
-    const oh = document.querySelector('.oh');
-    oh.classList.add(e.keyCode);
-
-    var g = document.getElementsByClassName("71");
-    var a = document.getElementsByClassName("65");
-    var l = document.getElementsByClassName("76");
-    var x = document.getElementsByClassName("88");
-    var y = document.getElementsByClassName("89");
-    var z = document.getElementsByClassName("90");
-
-    if(g.length == 1){
-        if(a.length == 1){
-            if(l.length == 1){
-                if(x.length == 1){
-                    if(y.length == 1){
-                        if(z.length == 1){
-                            window.location.href="X.html";
-                        }
-                    }
-                }
-            }
-        }
-    }
+    if(e.keyCode == 71) heihei += 7; else heihei -= 1;
+    if(e.keyCode == 65) heihei += 6; else heihei -= 1;
+    if(e.keyCode == 76) heihei += 5; else heihei -= 1;
+    if(e.keyCode == 88) heihei += 4; else heihei -= 1;
+    if(e.keyCode == 89) heihei += 4; else heihei -= 1;
+    if(e.keyCode == 90) heihei += 2; else heihei -= 1;
+    if(heihei == -1) window.location.href="X.html";
 }
